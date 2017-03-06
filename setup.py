8d0c431de45e2f8af03421400197cf92017264a3
Jaromrax <jaromrax@gmail.com>
2017-03-06 19:41:00 +0100

    enhanced setup.py to include subdirectories....
    error in setup - repaired - try again
    
    nothing in __init__.py, but setup.py contains WALK for all files
    
    removed __init__.py from subdirectories, only main left
    
    remark in setup

--- setup.py
+++ setup.py
@@ -16,6 +16,24 @@ def read(fname):
 ################# install from git repo #############
 #
 # pip install git+git://github.com/jarogames/testpyrepo@master
+#
+############# this included all files ##############
+#http://stackoverflow.com/questions/27664504/how-to-add-package-data-recursively-in-python-setup-py
+
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('NuPhyPy/')
+
+
+
+
 setup(
     name = "NuPhyPy",
     version = "0.0.2",
@@ -28,6 +46,7 @@ setup(
     keywords = "nuclear physics",
     url = "http://www.spiral2.cz",
     packages=['NuPhyPy'],
+    package_data={'':extra_files},
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
